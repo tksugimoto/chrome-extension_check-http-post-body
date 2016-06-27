0ada791324ceb2e72b9f293d7dc59b50a1698240
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-06-27 18:22:29 +0900

    [Add] POST内容確認

---
+++ background.js
@@ -0,0 +1,24 @@
+﻿
+chrome.webRequest.onBeforeRequest.addListener(details => {
+	var method = details.method;
+	if (method !== "GET") {
+		var url = details.url;
+		var requestBody = details.requestBody || {};
+		var host = url.match(/:[/][/]([^/]+)/)[1];
+		console.log(method, host, {
+			url: url,
+			type: details.type,
+			raw: requestBody.raw,
+			formData: requestBody.formData,
+			details: details
+		});
+	}
+}, {
+	urls: [
+		"http://*/*",
+		"https://*/*"
+	]
+}, [
+	"blocking",
+	"requestBody"
+]);
