dfca273dbcb5d4b3804765469a36520c7063f972
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-07-18 11:41:41 +0900

    [Update] raw POST時の内容をDecodeして表示する

--- background.js
+++ background.js
@@ -4,11 +4,21 @@ chrome.webRequest.onBeforeRequest.addListener(details => {
 	if (method !== "GET") {
 		var url = details.url;
 		var requestBody = details.requestBody || {};
+		var raw = requestBody.raw;
+		if (raw) {
+			raw = raw.map(obj => {
+				if (obj.bytes) {
+					return arrayBuffer2string(obj.bytes);
+				} else {
+					return obj;
+				}
+			});
+		}
 		var host = url.match(/:[/][/]([^/]+)/)[1];
 		console.log(method, host, {
 			url: url,
 			type: details.type,
-			raw: requestBody.raw,
+			raw: raw,
 			formData: requestBody.formData,
 			details: details
 		});
@@ -22,3 +32,9 @@ chrome.webRequest.onBeforeRequest.addListener(details => {
 	"blocking",
 	"requestBody"
 ]);
+
+
+function arrayBuffer2string(arrayBuffer) {
+	var uint8Array = new Uint8Array(arrayBuffer);
+	return new TextDecoder("utf-8").decode(uint8Array);
+}
